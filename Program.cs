459d3fed653706e0ef031bfe352ab03cf87d453d
Fernandounuts <fernado.balieiro0580@hotmail.com>
2023-10-31 16:29:02 -0300

    misc

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using CaminhadasAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => {
+    options.UseNpgsql(builder
+        .Configuration
+        .GetConnectionString("AppDb"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
