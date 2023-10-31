e0e442aeb18c0d7d8411efc55a1cb9ac5ef228b8
Fernandounuts <fernado.balieiro0580@hotmail.com>
2023-10-31 17:51:06 -0300

    criado dto para regiao

---
+++ Models/DTOs/RegionDTO.cs
@@ -0,0 +1,8 @@
+namespace CaminhadasAPI.Models.DTOs; 
+
+public class RegionDTO {
+    // public Guid Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string? ImageUrl { get; set; }
+}
