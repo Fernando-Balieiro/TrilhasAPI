c6ddcad102179c50cd5ce9cf53cb2ef294b30f70
Fernandounuts <fernado.balieiro0580@hotmail.com>
2023-10-31 19:18:00 -0300

    migration nova

---
+++ Migrations/20231031220311_TrocadoParametroDescription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CaminhadasAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class TrocadoParametroDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Descrition",
+                table: "Walks",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Walks",
+                newName: "Descrition");
+        }
+    }
+}
