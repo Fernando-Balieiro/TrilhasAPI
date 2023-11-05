8aa81c8b69063d7cd7faaf6d31acc50642b9804c
Fernandounuts <fernado.balieiro0580@hotmail.com>
2023-11-04 23:23:53 -0300

    dtos criados para funcionamento da API

--- Models/DTOs/UpdateWalkDto.cs
+++ Models/DTOs/UpdateWalkDto.cs
@@ -1,16 +1,10 @@
-using CaminhadasAPI.Models.Domain;
-
 namespace CaminhadasAPI.Models.DTOs; 
 
-public class WalkDto {
-    public Guid Id { get; set; }
+public class UpdateWalkDto {
     public string? Name { get; set; }
     public string? Description { get; set; }
     public double LengthInKm { get; set; }
     public string? WalkImageUrl { get; set; }
-
     public Guid? RegionId { get; set; }
-    public RegionDto? Region { get; set; }
     public Guid? DifficultyId { get; set; }
-    public DifficultyDto? Difficulty { get; set; }
 }
