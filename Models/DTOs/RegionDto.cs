5554580fc931bd92b5cc959ad61617d329797f02
Fernandounuts <fernado.balieiro0580@hotmail.com>
2023-11-04 23:23:10 -0300

    misc

--- Models/DTOs/RegionDto.cs
+++ Models/DTOs/RegionDto.cs
@@ -1,6 +1,6 @@
 namespace CaminhadasAPI.Models.DTOs; 
 
-public class RegionDTO {
+public class RegionDto {
     // public Guid Id { get; set; }
     public string? Code { get; set; }
     public string? Name { get; set; }
