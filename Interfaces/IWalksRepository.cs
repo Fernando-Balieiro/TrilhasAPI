22a8bb35a213b274d6d1303007cf87582eec8970
Fernandounuts <fernado.balieiro0580@hotmail.com>
2023-11-05 21:58:50 -0300

    adicionado a opção de dar paging e sorting nas trilhas

--- Interfaces/IWalksRepository.cs
+++ Interfaces/IWalksRepository.cs
@@ -1,11 +1,14 @@
 using CaminhadasAPI.Models.Domain;
 using CaminhadasAPI.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CaminhadasAPI.Interfaces; 
 
 public interface IWalksRepository {
     Task<Walk> CreateWalk(Walk walk);
-    Task<List<Walk>?> GetAllWalks();
+    Task<List<Walk>?> GetAllWalks
+        (string? filterOn = null, string? filterQuery = null, string? sortBy = null, bool isAscending = true,
+        int pageNumber = 1, int pageSize = 1000);
     Task<Walk?> GetById(Guid id);
     Task<Walk?> UpdateAsync(Guid id, Walk walk);
     Task<Walk?> DeleteAsync(Guid id);
