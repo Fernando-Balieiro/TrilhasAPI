43294975dba0a0875270f46e9f2410873e496aca
Fernandounuts <fernado.balieiro0580@hotmail.com>
2023-11-05 21:59:38 -0300

    autorização

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using System.Text;
 using CaminhadasAPI.Data;
 using CaminhadasAPI.Interfaces;
 using CaminhadasAPI.Mappings;
 using CaminhadasAPI.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,9 +18,28 @@ builder.Services.AddDbContext<AppDbContext>(options => {
         .GetConnectionString("AppDb"));
 });
 
+builder.Services.AddDbContext<AuthDbContext>(options => {
+    options.UseNpgsql(builder
+        .Configuration
+        .GetConnectionString("AppWalksAuthDb"));
+});
+
 builder.Services.AddScoped<IRegionRepository, RegionRepository>();
 builder.Services.AddScoped<IWalksRepository, SqlWalkRepository>();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+        option.TokenValidationParameters = new TokenValidationParameters() {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding
+                    // Tratar para null caso por algum motivo não tenha as chaves necessárias em appsettings.json
+                .UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -32,6 +54,7 @@ if (app.Environment.IsDevelopment()) {
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
